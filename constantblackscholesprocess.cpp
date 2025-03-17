d7aff0cea73a5a09c91ea738c1d13338cb764a52
Faberra57 <thomas.favre57@gmail.Com>
2025-03-17 17:57:58 +0100

    Finalize implementing ConstantBlackScholesProcess"

--- constantblackscholesprocess.cpp
+++ constantblackscholesprocess.cpp
@@ -1,3 +1,26 @@
+// ConstantBlackScholesProcess.cpp
 
 #include "constantblackscholesprocess.hpp"
 
+namespace QuantLib {
+
+    // Constructor with constant parameters for r (rate), sigma (volatility), d (dividend yield) and x0 (initial value)
+    ConstantBlackScholesProcess::ConstantBlackScholesProcess(Real r, Real sigma, Real d, Real x0)
+        : rate_(r), volatility_(sigma), dividendYield_(d), x0_(x0) {}
+
+    // Drift function: adjusted for the dividend yield
+    Real ConstantBlackScholesProcess::drift(Time t, Real x) const {
+        return (rate_ - dividendYield_) * x; // Drift = (r - q) * x
+    }
+
+    // Diffusion function: returns the diffusion term (constant volatility)
+    Real ConstantBlackScholesProcess::diffusion(Time t, Real x) const {
+        return volatility_ * x; // Diffusion = sigma * x
+    }
+
+    // Method to return the initial value of the process
+    Real ConstantBlackScholesProcess::x0() const {
+        return x0_; // Initial value of the process
+    }
+
+}
--- constantblackscholesprocess.hpp
+++ constantblackscholesprocess.hpp
@@ -1,34 +1,28 @@
+// ConstantBlackScholesProcess.hpp
+
+#ifndef CONSTANTBLACKSCHOLESPROCESS_HPP
+#define CONSTANTBLACKSCHOLESPROCESS_HPP
 
 #include <ql/stochasticprocess.hpp>
 
 namespace QuantLib {
 
     class ConstantBlackScholesProcess : public StochasticProcess1D {
-        public:
-            // Constructor with constant parameters for r (rate), sigma (volatility), d (dividend yield) and x0 (initial value)
-            ConstantBlackScholesProcess(Real r, Real sigma, Real d, Real x0)
-                : rate_(r), volatility_(sigma), dividendYield_(d), x0_(x0) {}
-    
-            // Overriding the pure virtual functions from StochasticProcess1D
-            Real x0() const override {
-                return x0_; // Initial value of the process
-            }
-    
-            // Drift function: adjusted for the dividend yield
-            Real drift(Time t, Real x) const override {
-                return (rate_ - dividendYield_) * x; // Drift = (r - q) * x
-            }
-    
-            // Diffusion function: returns the diffusion term (constant volatility)
-            Real diffusion(Time t, Real x) const override {
-                return volatility_ * x; // Diffusion = sigma * x
-            }
-    
-            // We can optionally define any other methods, but for now, these are sufficient for a constant Black-Scholes process
-        private:
-            Real rate_;          // Constant risk-free rate
-            Real volatility_;    // Constant volatility
-            Real dividendYield_; // Constant dividend yield
-            Real x0_;            // Initial asset value
-        };
+    public:
+        // Constructor with constant parameters for r (rate), sigma (volatility), d (dividend yield) and x0 (initial value)
+        ConstantBlackScholesProcess(Real r, Real sigma, Real d, Real x0);
+
+        // Overriding the pure virtual functions from StochasticProcess1D
+        Real x0() const override;
+        Real drift(Time t, Real x) const override;
+        Real diffusion(Time t, Real x) const override;
+
+    private:
+        Real rate_;          // Constant risk-free rate
+        Real volatility_;    // Constant volatility
+        Real dividendYield_; // Constant dividend yield
+        Real x0_;            // Initial asset value
+    };
 }
+
+#endif // CONSTANTBLACKSCHOLESPROCESS_HPP
